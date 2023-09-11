1a8b1f34337e2cacb19f6bdbcbc2cbc97ebfd00a
Janvi Arora <jjanvi.arora18@gmail.com>
2023-09-11 19:40:16 +0530

    basic set-up

---
+++ NetworkManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkManager
+//
+//  Created by Janvi Arora on 11/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
